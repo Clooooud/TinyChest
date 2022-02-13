faa54d0b45ec1e5d462253ed9e996b7414e953db
Clooooud <yohanbaret@hotmail.fr>
2022-02-13 15:48:00 +0100

    Added mineable tags and loot table

--- src/main/java/fr/cloud/tinychest/blocks/TinyChestBlock.java
+++ src/main/java/fr/cloud/tinychest/blocks/TinyChestBlock.java
@@ -4,6 +4,7 @@ import fr.cloud.tinychest.blocks.entities.BlockEntities;
 import fr.cloud.tinychest.blocks.entities.TinyChestBlockEntity;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
@@ -17,7 +18,7 @@ import net.minecraft.world.BlockView;
 public class TinyChestBlock extends ChestBlock {
 
     public TinyChestBlock() {
-        super(FabricBlockSettings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD), () -> BlockEntities.TINY_CHEST_BLOCK_ENTITY);
+        super(FabricBlockSettings.of(Material.WOOD).strength(2.5F).sounds(BlockSoundGroup.WOOD), () -> BlockEntities.TINY_CHEST_BLOCK_ENTITY);
     }
 
     @Override
